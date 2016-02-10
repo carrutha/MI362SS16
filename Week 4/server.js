d95f754ef5ed73bdd0878524d9e1e8494b78e863
carrutha <carrutha@msu.edu>
2016-02-10 09:02:56 -0500

    server.js week 4
    Updates to server.js to make hello world a header and a paragraph after

---
+++ Week 4/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer( (request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<h1>Hello World</h1>');
+  response.end('This is a node app!')
+}).listen(8124);
+
+console.log('Server running at http://127.0.0.1:8124/');
